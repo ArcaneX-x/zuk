4ab6073048b5517aa490f93be1b26e6e2f15d954
tee_zed <tee0zed@gmail.com>
2023-07-20 20:39:36 +0600

    Add source_message? and child_message? methods

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,13 @@ class Message < ApplicationRecord
   belongs_to :messagable, polymorphic: true
   validates :body, presence: true
 
-  # TODO add source_message_id column type as ID type
+  # TODO add source_message_id ID type column
+
+  def source_message?
+    source_message_id.blank?
+  end
+
+  def child_message?
+    !source_message?
+  end
 end
